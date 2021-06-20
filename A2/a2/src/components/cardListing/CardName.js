cd3b756bde5792c41376418d00c4abcfe9c4228f
Darvey <darvey6@hotmail.com>
2021-06-19 23:04:52 -0700

    Assignment 3

--- A2/a2/src/components/cardListing/CardName.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CardName = ({name}) => {
-    return <h2 className="URLCardName">{name}</h2>;
-};
-
-export default CardName;
--- A2/a2/src/components/cardListing/CardRating.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CardRating = ({rating}) => {
-    const ratingContent = rating.toLocaleString();
-    return <h4 className="URLCardRating">{ratingContent + " Star(s)"}</h4>;
-};
-
-export default CardRating;
